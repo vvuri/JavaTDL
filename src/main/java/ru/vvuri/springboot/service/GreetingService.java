0a4863d1a2381fe01ae17e3491c1e296a50a3270
Yuri Vinogradov <vvuri1978@gmail.com>
2021-10-18 14:06:51 +0300

    Add Spring test with resource file

--- src/main/java/ru/vvuri/springboot/service/GreetingService.java
+++ src/main/java/ru/vvuri/springboot/service/GreetingService.java
@@ -1,12 +1,17 @@
 package ru.vvuri.springboot.service;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 public class GreetingService {
 
+    // value in resources/application.yml
+    @Value("${app.services.greeting.message}")
+    private String greeting;
+
     public String greeting() {
-        return "Hello World";
+        return greeting;
     }
 
 }
--- src/test/java/ru/vvuri/springboot/service/GreetingServiceTest.java
+++ src/test/java/ru/vvuri/springboot/service/GreetingServiceTest.java
@@ -2,6 +2,7 @@ package ru.vvuri.springboot.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
@@ -10,6 +11,7 @@ import ru.vvuri.springboot.SpringBootMyApplication;
 
 import static org.testng.Assert.*;
 
+@ActiveProfiles("test")  // find resources/application-test.yml and overwrite on application.yml
 @SpringBootTest(classes = SpringBootMyApplication.class)
 public class GreetingServiceTest extends AbstractTestNGSpringContextTests {
 
@@ -26,7 +28,7 @@ public class GreetingServiceTest extends AbstractTestNGSpringContextTests {
 
     @Test
     public void  testGreeting() {
-        String expectedMessage = "Hello World";
+        String expectedMessage = "Hello World test";
         assertEquals(expectedMessage, this.greetingService.greeting());
     }
 }
